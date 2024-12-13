4c0e4205530f59a7c57bf1e81b7e03fc78f2f458
demgerall <demgerall@mail.ru>
2024-12-13 21:40:53 +0700

    something new...

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { App } from '@/app/App';
@@ -8,10 +8,10 @@ import { store } from '@/app/store/store';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 );
