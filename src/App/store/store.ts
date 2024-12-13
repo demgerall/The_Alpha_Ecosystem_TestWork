ff897b42555c619ec6d97f6693739e2ae13b1424
demgerall <demgerall@mail.ru>
2024-12-13 21:30:32 +0700

    Repair something...

--- src/App/store/store.ts
+++ src/App/store/store.ts
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { categoriesSlice, productsSlice, totalSlice } from '@/features';
 import { favouritesSlice } from '@/entities';
-// import { favouriteReducer } from '@/entities';
 
 export const store = configureStore({
     reducer: {
